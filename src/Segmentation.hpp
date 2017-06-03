a6d828429940103db11b72b53cc70cb499d37d9d
Łukasz Neumann <fuine@riseup.net>
2017-06-03 14:41:17 +0200

    Segmentation skeleton

---
+++ src/Segmentation.hpp
@@ -0,0 +1,9 @@
+#ifndef SEGMENTATION_HPP
+#define SEGMENTATION_HPP
+
+#include "opencv2/core/core.hpp"
+#include <vector>
+
+typedef std::vector<cv::Rect> Segments;
+
+#endif /* ifndef SEGMENTATION_HPP */
